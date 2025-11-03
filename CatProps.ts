5c178e92181b37cdefa5ae14b7723283c703dfd2
Sofia Ford <scfordw@bu.edu>
2025-11-03 18:34:39 -0500

    Creating a prop type for the cat images

---
+++ CatProps.ts
@@ -0,0 +1,10 @@
+export type CatProps = {
+    id: string,
+    width: number,
+    height: number,
+    url: string,
+    name: string,
+    temperament: string,
+    origin: string,
+    lifespan: string
+}
